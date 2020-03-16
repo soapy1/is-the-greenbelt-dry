6e48c0a7f7b19be2badf8c93471c56976b6f573f
sophia <scastellarin95@gmail.com>
2020-03-16 13:54:46 -0500

    Initial guess at dryness heuristic

--- is_the_greenbelt_dry/predict.py
+++ is_the_greenbelt_dry/predict.py
@@ -12,9 +12,27 @@ STATION_ID = "KTXAUSTI90"
 def main():
     ws = WeatherStation(API_KEY, STATION_ID)
     current_conditions = ws.get_current_conditions()
-    target_date = date(2020, 2, 11)
+    target_date = date(2020, 3, 9)
     current_weather_features = ws.get_weather_features(target_date)
-    print(current_weather_features)
+
+    latest_data_point = current_weather_features[-1]
+    # It's currently raining
+    if latest_data_point.get("current_precip_rate") > 0:
+        print("currently raining, no greenbelt")
+    # It's currently not raining
+    else:
+        # It didn't rain yesterday
+        if latest_data_point.get("previous_day_percip_total") == 0:
+            print("hasn't rained in  while, greenbelt is probably dry")
+        # It did rain yesterday
+        else:
+            # High temp + high solar radiation means the rock will dry well
+            if (latest_data_point.get("last_4_dewpt_avg") < latest_data_point.get("last_4_temp_avg")) and (latest_data_point.get("last_4_solar_radiation_high") > 400):
+                print("it recently rained, but there is a change that the greenbelt is dry enough")
+            else:
+                print("it recently rained and hasn't been very sunny, the greenbelt is probably wet")
+
+
 
 if __name__ == '__main__':
     main()
