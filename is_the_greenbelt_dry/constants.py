ef9e65009c60106d64e2346907c423b7ceb276f8
sophia <scastellarin95@gmail.com>
2020-03-16 15:13:45 -0500

    Refactor predict

--- is_the_greenbelt_dry/constants.py
+++ is_the_greenbelt_dry/constants.py
@@ -1,3 +1,5 @@
 API_BASE_URL = "https://api.weather.com"
 HISTORY_ENDPOINT = "/v2/pws/history/hourly"
 CURRENT_ENDPONT = "/v2/pws/observations/current"
+# https://www.wunderground.com/dashboard/pws/KTXAUSTI90
+SOUTH_AUSTIN_STATION_ID = "KTXAUSTI90"
--- is_the_greenbelt_dry/predict.py
+++ is_the_greenbelt_dry/predict.py
@@ -1,16 +1,14 @@
 import os
 from datetime import date
 from weather_station import WeatherStation
+from constants import SOUTH_AUSTIN_STATION_ID
 
 
 API_KEY = os.environ.get("WEATHER_API_KEY")
-# Station for Bouldin - South Austin
-# https://www.wunderground.com/dashboard/pws/KTXAUSTI90
-STATION_ID = "KTXAUSTI90"
 
 
-def main():
-    ws = WeatherStation(API_KEY, STATION_ID)
+def predict_if_greenbelt_dry():
+    ws = WeatherStation(API_KEY, SOUTH_AUSTIN_STATION_ID)
     current_conditions = ws.get_current_conditions()
     target_date = date(2020, 3, 9)
     current_weather_features = ws.get_weather_features(target_date)
@@ -38,7 +36,3 @@ def main():
                 print(
                     "it recently rained and hasn't been very sunny, the greenbelt is probably wet"
                 )
-
-
-if __name__ == "__main__":
-    main()
